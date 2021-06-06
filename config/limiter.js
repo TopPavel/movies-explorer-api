a0459229c75432ab4705370496241f2b050ee313
Pavel Toporov <pavel.toporov@raiffeisen.ru>
2021-06-06 22:42:12 +0300

    [NF] create diploma project (api) with yandex practicum

--- config/limiter.js
+++ config/limiter.js
@@ -2,5 +2,5 @@ const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
 });
